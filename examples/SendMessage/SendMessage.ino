09861e74a0ff7701577a513e325a2d53a270ec99
Tomer Klein <tomer.klein@gmail.com>
2024-04-07 14:25:49 +0300

    Update example to contain wifi connection

--- examples/SendMessage/SendMessage.ino
+++ examples/SendMessage/SendMessage.ino
@@ -1,13 +1,33 @@
+#include <Arduino.h>
+#include <WiFi.h>
 #include <GreenApi.h>
 
-GreenApi greenApi("YourInstanceID", "YourInstanceToken");
+const char* ssid = "YourWiFiSSID";
+const char* password = "YourWiFiPassword";
+const char* instanceId = "YourInstanceId";
+const char* instanceToken = "YourInstanceToken";
+const char* target = "11001234567@c.us";
+const char* message = "Hello, this is a test message from my new library!";
+
+// Create an instance of the GreenApi class
+GreenApi greenApi(instanceId, instanceToken);
 
 void setup() {
   Serial.begin(115200);
-  // Wait for serial monitor to open
-  while (!Serial);
+  delay(1000);
 
-  greenApi.sendMessage("YourChatId", "Hello, this is a test message from my new library!");
+  // Connect to Wi-Fi
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to Wi-Fi!");
+
+  // Send message using GreenApi
+  Serial.println("Sending message...");
+  greenApi.sendMessage(target, message);
 }
 
 void loop() {
